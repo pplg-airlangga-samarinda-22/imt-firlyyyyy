d106d433dcab40c2ea2cc6353864c6915f4b07f7
firlyyyyy <firlyalfaridzi88@gmail.com>
2025-02-04 09:26:45 +0800

    imt

--- imt.py
+++ imt.py
@@ -8,15 +8,15 @@ print(hasil)
 
 if hasil <= 17.0:
     print('Kategori : Kurus ringan')
-    
+
 elif hasil <= 18.4:
     print('Kategori : Kurus berat')
-    
-elif hasil  <= 25.0:
+
+elif hasil <= 25.0:
     print('Kategori : Normal')
-    
+
 elif hasil <= 27.0:
     print('Kategori : Gemuk ringan')
-    
+
 elif hasil >= 27:
     print('Kategori : Gemuk berat')
